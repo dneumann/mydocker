c2fb758c670000493fdb4ce94e7afab5382817a3
Dennis Neumann <neumann@sub.uni-goettingen.de>
2015-10-19 13:10:55 +0000

    Add docker install help for Mint 17

---
+++ help/mint17-docker.sh
@@ -0,0 +1,10 @@
+$ apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+echo deb https://apt.dockerproject.org/repo ubuntu-trusty main > /etc/apt/sources.list.d/docker.list
+
+apt-get update
+
+apt-cache policy docker-engine
+
+apt-get install docker-engine
+
