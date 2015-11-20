222fc03f08eecb7691f433c33bab8a75cd97b1a5
Dennis Neumann <neumann@sub.uni-goettingen.de>
2015-11-20 16:02:42 +0000

    Modify start script for Mac

--- mac-start.sh
+++ mac-start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-docker rm mydocker
+image=`head -1 docker-names.txt`
+container=`tail -1 docker-names.txt`
 
-docker run -v /Users/dn/mydocker:/home/dennis -it --privileged --name mydocker mydocker_image
+docker rm $container
+
+docker run -v /Users/dn/mydocker:/home/dennis -p 4200:4200 -it --privileged --name $container $image
 
